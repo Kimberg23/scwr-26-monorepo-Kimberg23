d8aada17da8ab5669f03ce73989430df7d856cfd
Kimberg23 <imbergk23@gmail.com>
2026-02-17 15:58:40 -0600

    Crypto ftw ! ! !

--- lab 5 - cryptoapp/src/App.jsx
+++ lab 5 - cryptoapp/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useEffect, useState } from 'react';
+import { get } from 'aws-amplify/api';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [coins, setCoins] = useState([]);
+
+  async function fetchCoins() {
+    const request = get({
+      apiName: 'cryptoapi',
+      path: '/coins',
+    })
+    const response = await request.response;
+    const data = await response.body.json();
+    setCoins(data.coins);
+  }
+
+  useEffect(() => {
+    fetchCoins()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>{coin.name} - {coin.symbol}</h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </>
   )
 }
