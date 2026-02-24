aaf2aa7f1f71d3912b8e3d71c124df3297b80c72
Kimberg23 <imbergk23@gmail.com>
2026-02-24 15:48:57 -0600

    Basic functionality completed...

--- lab 5 - cryptoapp/src/App.jsx
+++ lab 5 - cryptoapp/src/App.jsx
@@ -4,11 +4,18 @@ import { get } from 'aws-amplify/api';
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [input, updateInput] = useState({ limit: 5, start: 0})
+
+  function updateInputValues(type, value) {
+    updateInput({
+      ...input, [type]: value
+    })
+  }
 
   async function fetchCoins() {
     const request = get({
       apiName: 'cryptoapi',
-      path: '/coins',
+      path: `/coins?limit=${input.limit}&start=${input.start}`
     })
     const response = await request.response;
     const data = await response.body.json();
@@ -22,6 +29,9 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
+      <input onChange={e => updateInputValues('limit', e.target.value)} placeholder="limit"/>
+      <input onChange={e => updateInputValues('start', e.target.value)} placeholder="start"/>
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {coins.map((coin, index) => (
         <div key={index}>
           <h2>{coin.name} - {coin.symbol}</h2>
